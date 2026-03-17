97997cc9002970ac4e1cfc202f8943c9d57375f1
Billy V <bvinton66@gmail.com>
2026-03-17 17:37:41 +0000

    Update useUmpireAudio import path to correct hooks folder

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import KeyboardListener from "./components/KeyboardListener";
 import WebhookListener from "./components/WebhookListener";
 import ServeTimer from "./components/ServeTimer";
 import PlayerPanel from "./components/PlayerPanel";
-import useUmpireAudio from "../hooks/useUmpireAudio"; // Fix: Changed ./ to ../
+import useUmpireAudio from "../hooks/useUmpireAudio";
 import { MoreVertical } from "lucide-react";
 
 interface SavedMatch {
