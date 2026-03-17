238108668ee3490aa94f7c56c564f8b7f63fc5c0
Billy V <bvinton66@gmail.com>
2026-03-17 14:24:44 +0000

    Swapped to @ducanh2912/next-pwa to fix App Router offline caching bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
 };
 
-export default nextConfig;
-
+export default withPWA(nextConfig);
